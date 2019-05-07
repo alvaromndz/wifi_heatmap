52099ee630d47bb56051de47250f5bf817913287
Andrew Sterner <andrewjohnsterner@gmail.com>
2019-05-07 14:34:00 -0400

    fixed argparser and added json complete

--- scripts/ros_collect_aps.py
+++ scripts/ros_collect_aps.py
@@ -13,11 +13,6 @@ import tf2_ros
 import subprocess
 from time import sleep
 
-def kill_script(sig, frame):
-    """ close array at end of run """
-    print(']')
-    sys.exit(0)
-
 def scan():
     try:
         out = subprocess.check_output(['/home/turtlebot/catkin_ws/src/wifi_heatmap/scripts/scan_aps', 'wlp2s0'])
@@ -26,42 +21,44 @@ def scan():
     
     return out.encode('utf-8').strip().split('\n')
 
-def main(args):
-    signal.signal(signal.SIGINT, kill_script)
-
+def main(f, scans_per):
     rospy.init_node('wifi_strength')
-
     tfBuffer = tf2_ros.Buffer()
     listener = tf2_ros.TransformListener(tfBuffer)
 
 
     first = True
-    with open(args.outfile, 'w+') as f:
-    	f.write('[\n')
-        while not rospy.is_shutdown():
-            raw_input('Press enter to start scan...')
+    while not rospy.is_shutdown():
+        raw_input('Press enter to start scan...')
 
-            pos = tfBuffer.lookup_transform("map", 'base_link', rospy.Time(0), rospy.Duration(1.0))
-            for i in range(args.scans_per):
-                meas = scan()
-                if meas is not None:
-                    print('scan{}: {})} aps'.format(i, len(meas)))
-                else:
-                    print('scan{}: FAILED'.format(i))
-                
-                strengths += '['  + (', '.join(meas)) + ']'
+        pos = tfBuffer.lookup_transform("map", 'base_link', rospy.Time(0), rospy.Duration(1.0))
+        for i in range(scans_per):
+            meas = scan()
+            if meas is not None:
+                print('scan{}: {})} aps'.format(i, len(meas)))
+            else:
+                print('scan{}: FAILED'.format(i))
+            
+            strengths += '['  + (', '.join(meas)) + ']'
 
-                if not first:
-                    f.write(',\n')
-                f.write('{"x":%.2f, "y":%.2f, "aps":%s}' % (pos.transform.translation.x, pos.transform.translation.y, strengths))
-                first = False
+            if not first:
+                f.write(',\n')
+            f.write('{"x":%.2f, "y":%.2f, "aps":%s}' % (pos.transform.translation.x, pos.transform.translation.y, strengths))
+            first = False
                 
+def kill_script(f):
+    """ close array at end of run """
+    f.write(']\n')
+    sys.exit(0)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='collect locations and access point data using ros')
-    parser.add_argument('-n', metavar='scans_per', type=int, default=5,
+    parser.add_argument('-n', '--scans-per', type=int, default=5,
                             help='Number of scans to take per location')
     parser.add_argument('outfile', help='output filename')
 
     args = parser.parse_args()
-    main(args)
+    with open(args.outfile, 'w+') as f:
+        f.write('[\n')
+        signal.signal(signal.SIGINT, lambda sig, frame: kill_script(f))
+        main(f, args.scans_per)
