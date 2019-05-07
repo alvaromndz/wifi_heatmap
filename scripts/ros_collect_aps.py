5c39c3480e80c4d558a2026912f00e3357027e27
Andrew Sterner <andrewjohnsterner@gmail.com>
2019-05-07 13:40:14 -0400

    changed ros_collect_aps to take scans inplace

--- scripts/ros_collect_aps.py
+++ scripts/ros_collect_aps.py
@@ -6,6 +6,7 @@ import os
 import re
 import sys
 import signal
+import argparse
 import rospy
 import tf
 import tf2_ros
@@ -16,25 +17,48 @@ def kill_script(sig, frame):
     """ close array at end of run """
     print(']')
     sys.exit(0)
-signal.signal(signal.SIGINT, kill_script)
 
-rospy.init_node('wifi_strength')
-
-tfBuffer = tf2_ros.Buffer()
-listener = tf2_ros.TransformListener(tfBuffer)
-
-print('[')
-first = True
-while not rospy.is_shutdown():
-    sleep(0.5)
+def scan():
     try:
         out = subprocess.check_output(['/home/turtlebot/catkin_ws/src/wifi_heatmap/scripts/scan_aps', 'wlp2s0'])
     except subprocess.CalledProcessError:
-        continue
+        return None
+    
+    return out.encode('utf-8').strip().split('\n')
 
-    if not first:
-        print(',')
-    strengths = '['  + (', '.join(out.encode('utf-8').strip().split('\n'))) + ']'
-    pos = tfBuffer.lookup_transform("map", 'base_link', rospy.Time(0), rospy.Duration(1.0))
-    sys.stdout.write('{"x":%.2f, "y":%.2f, "aps":%s}' % (pos.transform.translation.x, pos.transform.translation.y, strengths))
-    first = False
+def main(args):
+    signal.signal(signal.SIGINT, kill_script)
+
+    rospy.init_node('wifi_strength')
+
+    tfBuffer = tf2_ros.Buffer()
+    listener = tf2_ros.TransformListener(tfBuffer)
+
+
+    print('[')
+    first = True
+    with open(args.outfile) as f:
+        while not rospy.is_shutdown():
+            input('Press enter to start scan...')
+
+            pos = tfBuffer.lookup_transform("map", 'base_link', rospy.Time(0), rospy.Duration(1.0))
+            for i in range(args.scans_per):
+                meas = scan()
+                if meas is not None:
+                    print(f'scan{i}: {len(meas)} aps')
+                else:
+                    print(f'scan{i}: FAILED')
+                
+                strengths += '['  + (', '.join(meas)) + ']'
+
+                if not first:
+                    f.write(',\n')
+                f.write('{"x":%.2f, "y":%.2f, "aps":%s}' % (pos.transform.translation.x, pos.transform.translation.y, strengths))
+                first = False
+                
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='collect locations and access point data using ros')
+    parser.add_argument('-n', metavar='scans_per', type=int, default=5,
+                            help='Number of scans to take per location')
+    parser.add_argument('outfile', help='output filename')
--- scripts/wifimap.py
+++ scripts/wifimap.py
@@ -1,7 +1,82 @@
 import json
+import heapq
+import pandas as pd
+
+def sim_cosine(z, x):
+    zmag = 0
+    xmag = 0
+    dot = 0
+    for mac, stren in z.items():
+        if mac in x:
+            dot += stren * x[mac]
+            xmag += x[mac]**2
+        zmag += stren**2
+    
+    if zmag == 0 or xmag == 0:
+        return 0
+
+    return dot / zmag**.5 / xmag**.5
+
+class Similarity(object):
+    def __init__(self, loc, sim):
+        self.loc = loc
+        self.sim = sim
+    
+    def __lt__(self, other):
+        return self.sim < other.sim
+    
+    def __eq__(self, other):
+        return self.sim == other.sim
+    
+    def __str__(self):
+        return str(self.loc + (self.sim,))
+
+
+
+class WifiMapOld(object):
+    def __init__(self, wmap_filename):
+        with open(wmap_filename, 'r') as f:
+            raw_wmap = json.load(f)
+        
+        self.wmap = {}
+        for z in raw_wmap:
+            aps = {ap['MAC'] : ap['strength'] for ap in z['aps']}
+            self.wmap[(z['x'], z['y'])] = aps
+        
+    def localize(self, meas):
+        """ 
+        returns best guess of location and error
+        meas should be list of dicts 
+        """
+        sims = [Similarity(loc, sim_cosine(meas, aps)) for loc, aps in self.wmap.items()]
+        most_sim = heapq.nlargest(10, sims)
+
+        for sim in most_sim:
+            print(sim)
 
 class WifiMap(object):
     def __init__(self, wmap_filename):
         with open(wmap_filename, 'r') as f:
-            self.wmap = json.load(f)
+            raw_wmap = json.load(f)
+        
+        macs = set()
+        for z in raw_wmap:
+            for ap in z['aps']:
+                macs.add(ap['MAC'])
+        
+
+
+    def localize(self, meas):
+        """ 
+        returns best guess of location and error
+        meas should be list of dicts 
+        """
+        sims = [Similarity(loc, sim_cosine(meas, aps)) for loc, aps in self.wmap.items()]
+        most_sim = heapq.nlargest(10, sims)
+
+        for sim in most_sim:
+            print(sim)
+
+    
+
 
