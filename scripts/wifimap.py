3e56b39989768a124e9b643f6b2ed1b06de7efd1
Andrew Sterner <andrewjohnsterner@gmail.com>
2019-05-09 11:58:03 -0400

    wifi localization class WifiMap written and tested

--- scripts/wifimap.py
+++ scripts/wifimap.py
@@ -1,82 +1,43 @@
 import json
-import heapq
+from collections import defaultdict
+import numpy as np
 import pandas as pd
-
-def sim_cosine(z, x):
-    zmag = 0
-    xmag = 0
-    dot = 0
-    for mac, stren in z.items():
-        if mac in x:
-            dot += stren * x[mac]
-            xmag += x[mac]**2
-        zmag += stren**2
-    
-    if zmag == 0 or xmag == 0:
-        return 0
-
-    return dot / zmag**.5 / xmag**.5
-
-class Similarity(object):
-    def __init__(self, loc, sim):
-        self.loc = loc
-        self.sim = sim
-    
-    def __lt__(self, other):
-        return self.sim < other.sim
-    
-    def __eq__(self, other):
-        return self.sim == other.sim
-    
-    def __str__(self):
-        return str(self.loc + (self.sim,))
-
-
-
-class WifiMapOld(object):
-    def __init__(self, wmap_filename):
-        with open(wmap_filename, 'r') as f:
-            raw_wmap = json.load(f)
-        
-        self.wmap = {}
-        for z in raw_wmap:
-            aps = {ap['MAC'] : ap['strength'] for ap in z['aps']}
-            self.wmap[(z['x'], z['y'])] = aps
-        
-    def localize(self, meas):
-        """ 
-        returns best guess of location and error
-        meas should be list of dicts 
-        """
-        sims = [Similarity(loc, sim_cosine(meas, aps)) for loc, aps in self.wmap.items()]
-        most_sim = heapq.nlargest(10, sims)
-
-        for sim in most_sim:
-            print(sim)
+from sklearn.metrics.pairwise import cosine_similarity
 
 class WifiMap(object):
-    def __init__(self, wmap_filename):
-        with open(wmap_filename, 'r') as f:
-            raw_wmap = json.load(f)
+    def __init__(self, wmap_file):
+        # load json
+        with open(wmap_file, 'r') as f:
+            wmap_json = json.load(f)
+
+        # build mapping of location -> list of measurements at location
+        wmap_dict = defaultdict(list)
+        for z in wmap_json:
+            aps = pd.Series({ap['MAC'] : ap['strength'] for ap in z['aps']})
+            if len(aps) > 1: # if only one probably bad scan
+                wmap_dict[(z['x'], z['y'])].append(aps)
+
+        self.wmap_dict = {loc : pd.DataFrame(aps).mean() 
+                             for loc, aps in wmap_dict.items()}
         
-        macs = set()
-        for z in raw_wmap:
-            for ap in z['aps']:
-                macs.add(ap['MAC'])
-        
-
-
-    def localize(self, meas):
-        """ 
-        returns best guess of location and error
-        meas should be list of dicts 
-        """
-        sims = [Similarity(loc, sim_cosine(meas, aps)) for loc, aps in self.wmap.items()]
-        most_sim = heapq.nlargest(10, sims)
-
-        for sim in most_sim:
-            print(sim)
-
+        self.wmap = pd.DataFrame(self.wmap_dict)
+        self.wmap.fillna(0., inplace=True)
+        self.locs = np.array([[x, y] for x, y in self.wmap.columns])
     
+    def probable_location(self, meas, n=5):
+        if n <= 0 or n > self.wmap.columns.size:
+            n = self.wmap.columns.size
 
+        ssim = self.p_loc_meas(meas)
+        most_likely = np.argpartition(-ssim, n-1)[:n]
 
+        weights = ssim[most_likely]        
+        norm = np.linalg.norm(weights, ord=1)
+        plocs = weights / norm if norm > 0 else weights
+
+        loc = (self.locs[most_likely, :] * plocs[:, None]).sum(axis=0)
+        return loc
+    
+    def p_loc_meas(self, meas):
+        X, Y = self.wmap.align(meas, fill_value=0, axis=0)
+        return cosine_similarity(X.T, Y[None, :]).flatten()
